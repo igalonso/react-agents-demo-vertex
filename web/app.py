dcdcb9d5641fb1f5a466d6524775665c49222a3c
igalonso <igalonso@gmail.com>
2023-12-13 16:22:19 +0100

    images in application

--- web/app.py
+++ web/app.py
@@ -31,19 +31,33 @@ st.markdown(
     f'<div>Welcome to the ReAct! We are going to do an example of a nice job offer to a candidate. For that we need to do some steps:<ul><li>Our recruiter agent will gather information about the candidate and the company using Tools.</li><li>That information will be shared with the HR department who is resposible to allocate budget for the salary.</li><li>With this information, the recruiter is going to draft an email to the candidate to explaion the position and the salary offer.</li></ul>LETS GO!</div>',
     unsafe_allow_html=True
 )
+st.image("web/img/diagram.png", width=700)
 
 full_name = st.text_input("Full Name of Candidate", value="Ignacio Garcia")
 company_name = st.text_input("Full Name of the company offering", value="Nintendo")
 position=st.text_input("Full Name of the postion offered",value="Solutions Architect")
+
 testing = st.checkbox("Testing", value=True)
 model_for_information_gathering = st.selectbox("Select a model for information gathering", ("text-bison@002","text-bison@001", "text-unicorn","gemini-pro"))
-model_for_hr_salary_decision = st.selectbox("Select a model for HR salary decision", ("text-bison@001","text-bison@002", "text-unicorn","gemini-pro"))
-model_for_email_draft = st.selectbox("Select a model for email draft", ("text-unicorn", "text-bison@001","text-bison@002","gemini-pro"))
+model_for_hr_salary_decision = st.selectbox("Select a model for HR salary decision", ("text-bison@001","text-bison@002", "text-unicorn"))
+model_for_email_draft = st.selectbox("Select a model for email draft", ("text-unicorn", "text-bison@001","text-bison@002"))
+temperature_slider = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
 verbose = False
-temp = 0
-
-
 generate = st.button("Run Agent🤖")
+
+#info_agent = st.image('img/info_agent.gif')
+file_ = open(os.getcwd()+"/web/img/info_agent.gif", "rb")
+contents = file_.read()
+info_agent_image = base64.b64encode(contents).decode("utf-8")
+file_.close()
+file_ = open(os.getcwd()+"/web/img/hr_agent.gif", "rb")
+contents = file_.read()
+hr_agent_image = base64.b64encode(contents).decode("utf-8")
+file_.close()
+file_ = open(os.getcwd()+"/web/img/email_agent.gif", "rb")
+contents = file_.read()
+email_agent_image = base64.b64encode(contents).decode("utf-8")
+file_.close()
 #stop = st.button("Stop Agent🤖")
 
 if generate:
@@ -52,9 +66,30 @@ if generate:
         os.environ["VERTEX_MODEL_SALARY"] = model_for_hr_salary_decision
         os.environ["VERTEX_MODEL_EMAIL"] = model_for_email_draft
         os.environ["TESTING"] = str(testing)
-        agent.recruiter_start(position, company_name, full_name, verbose)
+        #agent.recruiter_start(position, company_name, full_name, verbose, temperature_slider)
+        info_agent_md = st.markdown(
+            f'<div class="gif_holder"><img  src="data:image/gif;base64,{info_agent_image}" alt="Info Agent GIF" width="700px !important" style="border: solid red;"></div>',
+            unsafe_allow_html=True,
+        )
+        candidate_summary = agent.recruiter_personal_inspection(position, company_name, full_name, verbose)
+        info_agent_md.empty()
+        hr_agent_md = st.markdown(
+            f'<div class="gif_holder"><img  src="data:image/gif;base64,{hr_agent_image}" alt="HR Agent GIF" width="700px" style="border: solid red;"></div>',
+            unsafe_allow_html=True,
+        )
+        #st.write(candidate_summary)
+        salary_offer = agent.hr_salary_estimation(candidate_summary, verbose)
+        #st.write("- **Salary offered:** "+salary_offer)
+        candidate_summary = candidate_summary + "\n- Salary Offer: " + salary_offer + "\n"
+        hr_agent_md.empty()
+        email_agent_md = st.markdown(
+            f'<div class="gif_holder"><img  src="data:image/gif;base64,{email_agent_image}" alt="HR Agent GIF" width="700px" style="border: solid red;"></div>',
+            unsafe_allow_html=True,
+        )
+        agent.recruiter_email_creator(candidate_summary,full_name, company_name, position, verbose)
+        email_agent_md.empty()
         st.balloons()
-        st.success("Agent finished! - Now you can access your mail to find out the draft offer")
+        st.success("Agent finished! - Now you can access your mail to find out the draft offer")  
         st.write("[gmail link to drafts](https://mail.google.com/mail/u/0/#drafts)")
 # if stop:
 #     st.stop()
